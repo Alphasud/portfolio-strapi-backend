75121a0817535aa8122788983a49aa7a0209ce7a
charlie <kislapsky@gmail.com>
2021-11-17 15:07:40 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '47388c70ca575cca845a8b234cbbe309'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '524fb491-0fe6-41af-8451-1b4862a191fd'
+};
